e0c8a609b7c481575ce8e473272f7a83a14f78fd
annieherieau <annie.herieau@gmail.com>
2024-02-12 16:38:24 +0100

    routes gossips

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,16 @@
+class GossipsController < ApplicationController
+  def index
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'gossips#index'
+
+  get 'gossips/new'
+  get 'gossips/edit'
+  get 'gossips/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
