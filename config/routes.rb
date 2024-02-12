8cff8207e82f250960c2a4415f872c3b3f64b6a1
annieherieau <annie.herieau@gmail.com>
2024-02-12 16:49:31 +0100

    routes contact, team

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  root 'gossips#index'
+  get 'contact', to: 'static_pages#contact'
+  get 'team', to: 'static_pages#team'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'team', to: 'application#team'
 
   get 'gossips/new'
   get 'gossips/edit'
   get 'gossips/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'gossips#index'
 end
