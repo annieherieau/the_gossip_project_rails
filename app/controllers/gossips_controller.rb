8bfe75f25b5fd86bd2b6538fb637ec118d350002
annieherieau <annie.herieau@gmail.com>
2024-02-14 11:29:11 +0100

    form_with Create @gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,12 +1,13 @@
 class GossipsController < ApplicationController
   def index
     # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
-    @gossips_all = Gossip.all
+    @gossips_all = Gossip.all.sort_by{|g| -g.id}
   end
 
   def show
     # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
     @gossip = Gossip.find(params[:id])
+    @comments = Comment.where(commented_gossip_id: @gossip.id) 
   end
 
   def new
@@ -30,10 +31,6 @@ class GossipsController < ApplicationController
     end
   end
 
-  def edit
-    # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
-    @gossip = Gossip.find(params[:id])
-  end
 
   def update
     # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
