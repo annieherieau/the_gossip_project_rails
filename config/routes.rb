ae1055cffff164dc54d5734e86bda966011bc771
annieherieau <annie.herieau@gmail.com>
2024-02-15 15:13:49 +0100

    sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # ROOT
@@ -27,9 +28,9 @@ Rails.application.routes.draw do
   # LIKES
   resources :likes, only: [:create, :destroy]
 
-
-
-  
+  # SESSIONS
+  resources :sessions, only: [:new, :create, :destroy]
+ 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
