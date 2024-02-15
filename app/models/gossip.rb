f4580cdd4978d1e4501c88f095d8eae8114de01e
annieherieau <annie.herieau@gmail.com>
2024-02-15 23:06:18 +0100

    gossips / comments / likes dependent: :destroy

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -5,10 +5,10 @@ class Gossip < ApplicationRecord
   has_many :gossip_tags
   has_many :tags, through: :gossip_tags
   # Table N-N
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :commenting_users, foreign_key: 'commenting_user_id', class_name: 'User', through: :comments
   # Table N-N
-  has_many :likes
+  has_many :likes, dependent: :destroy
   # todo hasm_many liking_user through likes
 
   # Validations
